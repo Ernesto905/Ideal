82ec9e7c00368279f8f69a0fa3ab7838daec7214
eernesto <eernesto@umich.edu>
2024-03-14 11:27:00 -0400

    Some frontend changes to Central agent, will now begin working on backend

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+# Frontend routes 
 @app.route("/")
 def home():
     return render_template('home/index.html')
@@ -10,7 +11,9 @@ def home():
 def builder():
     return render_template('builder/index.html')
 
-
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard/index.html')
+
+
+# Backend routes (OPTIONAL: If decide to Auth, utilize Json web tokens 🤢🤮)
