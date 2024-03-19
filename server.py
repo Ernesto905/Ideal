02e00bfab7303d9af8fe8bb8f287bebce43c72db
eernesto <eernesto@umich.edu>
2024-03-19 09:25:57 -0400

    Managed frontend to backend communication

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from llm.openai_utils import get_completion
+from openai import OpenAI
+import dotenv
+
+dotenv.load_dotenv()
+client = OpenAI()
 
 app = Flask(__name__)
 
@@ -17,3 +23,14 @@ def dashboard():
 
 
 # Backend routes (OPTIONAL: If decide to Auth, utilize Json web tokens 🤢🤮)
+@app.route("/generate", methods=["GET", "POST"])
+def generate():
+    print("This should work")
+    if request.method == "POST": 
+        response = get_completion(request.form["user_input"])
+        print(f"response is {response}")
+        return redirect(url_for("generate", result=response))
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
