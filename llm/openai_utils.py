0b2935562e5dccd491a52f27d19b50633ac234cf
eernesto <eernesto@umich.edu>
2024-03-21 10:53:33 -0400

    Added sessions functionality to store user input. Session variables are permeated across both frontend and backend

--- llm/openai_utils.py
+++ llm/openai_utils.py
@@ -13,7 +13,33 @@ def get_completion(form_input):
         {"role": "user", "content": form_input}
       ]
     )
-    print(f"SKJDFHSIUFGHDFUISDAHFDASUIFGDKSHFKCompletion is {completion}")
 
     return completion.choices[0].message
 
+def test_backend(session):
+    # Access session values
+    current_weight = session.get("current_weight")
+    ideal_weight = session.get("ideal_weight")
+    body_composition = session.get("body_composition")
+    archetype = session.get("archetype")
+    age = session.get("age")
+    sex = session.get("sex")
+    allergies = session.get("allergies")
+    diet = session.get("diet")
+    religion = session.get("religion")
+    anything_else_diet = session.get("anything_else_diet")
+    physical_impediments = session.get("physical_impediments")
+
+    session_values = f"Values are current weight: {current_weight}<br>" \
+                     f"ideal weight: {ideal_weight}<br>" \
+                     f"body composition: {body_composition}<br>" \
+                     f"archetype: {archetype}<br>" \
+                     f"age: {age}<br>" \
+                     f"sex: {sex}<br>" \
+                     f"allergies: {allergies}<br>" \
+                     f"diet: {diet}<br>" \
+                     f"religion: {religion}<br>" \
+                     f"anything else diet: {anything_else_diet}<br>" \
+                     f"physical impediments: {physical_impediments}"
+
+    return session_values
--- server.py
+++ server.py
@@ -1,36 +1,58 @@
-from flask import Flask, render_template, url_for, request, redirect
-from llm.openai_utils import get_completion
-from openai import OpenAI
-import dotenv
+from flask import Flask, render_template, url_for, request, redirect, session
+from llm.openai_utils import get_completion, test_backend
+from dotenv import load_dotenv
+import os
 
-dotenv.load_dotenv()
-client = OpenAI()
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SESSIONS_SECRET")
 
 # Frontend routes 
 @app.route("/")
 def home():
     return render_template('home/index.html')
 
-@app.route("/builder")
+@app.route("/builder", methods=["GET", "POST"])
 def builder():
+
+    if request.method == "POST":
+        session["current_weight"] = request.form.get("current_weight")
+        session["ideal_weight"] = request.form.get("ideal_weight")
+        session["body_composition"] = request.form.get("body_composition")
+        session["archetype"] = request.form.get("archetype")
+        session["age"] = request.form.get("age")
+        session["sex"] = request.form.get("sex")
+        session["allergies"] = request.form.get("allergies")
+        session["diet"] = request.form.get("diet")
+        session["religion"] = request.form.get("religion")
+        session["anything_else_diet"] = request.form.get("anything_else")
+        session["physical_impediments"] = request.form.get("physical_impediments")
+        return redirect("/dashboard")
     return render_template('builder/index.html')
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template('dashboard/index.html')
+    return render_template('dashboard/index.html', session=session)
 
 
 # Backend routes (OPTIONAL: If decide to Auth, utilize Json web tokens 🤢🤮)
-@app.route("/generate", methods=["GET", "POST"])
-def generate():
-    print("This should work")
-    if request.method == "POST": 
-        response = get_completion(request.form["user_input"])
-        print(f"response is {response}")
-        return redirect(url_for("generate", result=response))
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
 
+@app.route("/test_backend", methods=["GET"])
+def test_the_backend():
+    result = test_backend(session)
+    return result
+
+
+"""Commented out just in case we need it in the future"""
+# @app.route("/generate", methods=["GET", "POST"])
+# def generate():
+#     print("This should work")
+#     if request.method == "POST": 
+#         response = get_completion(request.form["user_input"])
+#         print(f"response is {response}")
+#         return redirect(url_for("generate", result=response))
+#     result = request.args.get("result")
+#     return render_template("index.html", result=result)
 
