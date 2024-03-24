8a668b538173e2be4a2868427bd3484fa54bf054
eernesto <eernesto@umich.edu>
2024-03-24 13:18:31 -0400

    pulled in changes from garv and dealt with merge conflict residue comments

--- llm/openai_utils.py
+++ llm/openai_utils.py
@@ -1,6 +1,7 @@
 from openai import OpenAI 
+import os 
 import dotenv
-
+from llm import nutrition_function
 dotenv.load_dotenv()
 
 client = OpenAI()
@@ -30,6 +31,8 @@ def test_backend(session):
     anything_else_diet = session.get("anything_else_diet")
     physical_impediments = session.get("physical_impediments")
 
+    # print(nutrition_function.main(current_weight, ideal_weight, body_composition, archetype, age, sex, allergies, diet))
+
     session_values = f"Values are current weight: {current_weight}<br>" \
                      f"ideal weight: {ideal_weight}<br>" \
                      f"body composition: {body_composition}<br>" \
@@ -41,5 +44,24 @@ def test_backend(session):
                      f"religion: {religion}<br>" \
                      f"anything else diet: {anything_else_diet}<br>" \
                      f"physical impediments: {physical_impediments}"
+
+    return session_values
+
+def test_backend_garv(session):
+    # Access session values
+    current_weight = session.get("current_weight")
+    ideal_weight = session.get("ideal_weight")
+    body_composition = session.get("body_composition")
+    archetype = session.get("archetype")
+    age = session.get("age")
+    sex = session.get("sex")
+    allergies = session.get("allergies")
+    diet = session.get("diet")
+    religion = session.get("religion")
+    anything_else_diet = session.get("anything_else_diet")
+    physical_impediments = session.get("physical_impediments")
+
+    session_values = nutrition_function.main(current_weight, ideal_weight, body_composition, archetype, age, sex, allergies, diet)
+    print(session_values)
 
     return session_values
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, session
-from llm.openai_utils import get_completion, test_backend
+from llm.openai_utils import get_completion, test_backend, test_backend_garv
 from dotenv import load_dotenv
 import os
 
@@ -43,6 +43,11 @@ def dashboard():
 def test_the_backend():
     result = test_backend(session)
     return result
+
+@app.route("/test_backend_garv", methods=["GET"])
+def test_the_backend_garv():
+    result_garv = test_backend_garv(session)
+    return result_garv
 
 
 """Commented out just in case we need it in the future"""
